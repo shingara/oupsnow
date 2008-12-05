a84e985970827b7293f2ea0faf897a5b65255380
Cyril Mougel <cyril.mougel@jtek.fr>
2008-12-05 18:20:33 +0100

    add some tickets generate by fixtures

--- spec/fixtures.rb
+++ spec/fixtures.rb
@@ -1,4 +1,10 @@
 Project.fixture {{
   :name => /\w+/.gen,
   :description => (0..3).of { /[:paragraph:]/.generate }.join("\n"),
+  :tickets => (0..10).of {Ticket.make}
+}}
+
+Ticket.fixture {{
+  :title => /\w+/.gen,
+  :description => (0..3).of { /[:paragraph:]/.generate }.join("\n"),
 }}
