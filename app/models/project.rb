57b26e1ca18b7142265f4626cfe1e643be77c817
Cyril Mougel <cyril.mougel@gmail.com>
2008-12-22 23:40:01 +0100

    add delete all ticket after destroy project

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,4 +10,12 @@ class Project
   has n, :users, :through => :members
   has n, :functions, :through => :members
 
+  after :destroy, :destroy_tickets
+
+  # Destroy all ticket depend on this project
+  # non needing when cascading come
+  def destroy_tickets
+    Ticket.all(:project_id => id).destroy!
+  end
+
 end
