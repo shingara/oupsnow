b34fdf1726199184243bf87f65620b2237bbe43b
Cyril Mougel <cyril.mougel@gmail.com>
2008-12-24 18:02:51 +0100

    update to merb 1.0.6.1

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,5 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.4"
+merb_gems_version = "1.0.6.1"
 dm_gems_version   = "0.9.8"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
@@ -24,3 +24,5 @@ dependency "dm-timestamps", dm_gems_version
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
 dependency "dm-sweatshop", dm_gems_version
+
+dependency "nokogiri", '1.0.7'
