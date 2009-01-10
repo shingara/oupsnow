841ea21bb806d069d6c6763d5c5f04e39b9970be
Cyril Mougel <cyril.mougel@gmail.com>
2009-01-10 23:32:56 +0100

    better view of ticket update

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,5 +1,44 @@
 module Merb
   module TicketsHelper
 
+    def update_field(prop)
+      "#{property_update(prop)} is change from '#{old_field(prop)}' to '#{new_field(prop)}'"
+    end
+
+    def property_update(prop)
+      case prop[0]
+      when :state_id
+        "Status"
+      when :tag_id
+        "Tag"
+      when :member_id
+        "Responsible"
+      when :title
+        "Title"
+      end
+    end
+
+    def old_field(prop)
+      transform_field(prop[0], prop[1])
+    end
+
+    def new_field(prop)
+      transform_field(prop[0], prop[2])
+    end
+
+    def transform_field(state, field)
+      return "" if field.nil?
+      case state
+      when :state_id
+        State.get(field).name
+      when :tag_id
+        Tag.get(field).name
+      when :member_id
+        Member.get(field).user_name
+      when :title
+        field
+      end
+    end
+
   end
 end # Merb
