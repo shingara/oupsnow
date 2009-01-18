6b3fa1f4f76819d30cdbf1a433753ec23906dd52
Cyril.mougel <cyril.mougel@gmail.com>
2009-01-19 00:24:55 +0100

    update merb to 1.0.7 to 1.0.8

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,5 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.7.1"
+merb_gems_version = "1.0.8"
 dm_gems_version   = "0.9.9"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
