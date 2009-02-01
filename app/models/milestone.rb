0b7a28bb576cf6c9ed93ab11b49df8d782ea7e6c
Cyril Mougel <cyril.mougel@gmail.com>
2009-02-01 13:31:44 +0100

    add milestone view

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -8,6 +8,8 @@ class Milestone
 
   belongs_to :project
 
+  has n, :tickets
+
   def write_event_create(user)
     Event.create(:eventable_class => self.class,
                  :eventable_id => id,
@@ -17,6 +19,20 @@ class Milestone
   end
 
   alias_method :title, :name
+
+  def percent_complete
+    return 0 if tickets.size == 0
+    ((ticket_open.to_f / tickets.size.to_f) * 100).to_f
+  end
+
+  def ticket_open
+    tickets.count(:state_id.not => State.first(:name => 'closed').id)
+  end
+
+  def ticket_closed
+    #TODO: define a closed state to all state
+    tickets.count(:state_id => State.first(:name => 'closed').id)
+  end
 
 
 end
