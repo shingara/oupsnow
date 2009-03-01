58a2f71fbc54a6bc2c6022b5167b10059c23a657
Cyril Mougel <cyril.mougel@gmail.com>
2009-03-01 17:58:13 +0100

    define css like min if only one tag by tag type

--- app/helpers/tags_part_helper.rb
+++ app/helpers/tags_part_helper.rb
@@ -8,7 +8,11 @@ module Merb
       tags.each { |key, value| max_count = value.size if value.size > max_count }
 
       tags.each do |tag_id, tagging|
-        index = ((tagging.size / max_count) * (classes.size - 1)).round
+        if max_count > 1
+          index = ((tagging.size / max_count) * (classes.size - 1)).round
+        else
+          index = 0
+        end
         yield Tag.get(tag_id), classes[index]
       end
     end
