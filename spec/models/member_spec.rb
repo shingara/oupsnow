40d5cc3ba5f2f7fcfdfe6a2c15bcc33d918269c4
Cyril Mougel <cyril.mougel@gmail.com>
2009-06-23 01:05:48 +0200

    avoid transaction

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -15,8 +15,6 @@ describe Member do
     it "should change member function" do
       project = Project.gen
       member = Member.gen!(:project_id => project.id)
-      transaction = DataMapper.repository(:default).adapter.pop_transaction
-      transaction.commit
       Member.change_functions({member.id => Function.admin.id}).should be_true
       member.reload.function.should == Function.admin
     end
@@ -26,19 +24,19 @@ describe Member do
       member_admin = project.members.first
       member_admin.function.should be_project_admin
       member = Member.gen!(:project_id => project.id, :user_id => User.gen.id)
+      another_member_admin = Member.gen!(:project_id => project.id, :user_id => User.gen.id)
+      another_member_admin.function_id = Function.admin.id
+      another_member_admin.save
       project.reload
       Function.gen! unless Function.not_admin # Need a function not admin
-      transaction = DataMapper.repository(:default).adapter.pop_transaction
-      transaction.commit
-      Member.change_functions({member_admin.id => Function.not_admin.id,
+      Member.change_functions({
+                               another_member_admin.id => Function.not_admin.id,
+                               member_admin.id => Function.not_admin.id,
                                member.id => Function.not_admin.id}).should be_false
       member_admin.reload.function.should be_project_admin
-      # Need delete all add before commit
-      Project.all.each {|pr| pr.destroy}
-      Member.all.each {|pr| pr.destroy}
-      project.destroy
-      member_admin.destroy
-      member.destroy
+      another_member_admin.reload.function.should be_project_admin
+      member.reload.function.should_not be_project_admin
+
     end
   end
 
