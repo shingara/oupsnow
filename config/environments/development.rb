4b27715d40dfa130a0be1140508e5ff42d3e3de0
Cyril Mougel <cyril.mougel@gmail.com>
2009-10-24 12:50:46 +0200

    update config directory to rails environement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,17 @@
-Merb.logger.info("Loaded DEVELOPMENT Environment...")
-Merb::Config.use { |c|
-  c[:exception_details] = true
-  c[:reload_templates] = true
-  c[:reload_classes] = true
-  c[:reload_time] = 0.5
-  c[:ignore_tampered_cookies] = true
-  c[:log_auto_flush ] = true
-  c[:log_level] = :debug
+# Settings specified here will take precedence over those in config/environment.rb
 
-  c[:log_stream] = STDOUT
-  c[:log_file]   = nil
-  # Or redirect logging into a file:
-  # c[:log_file]  = Merb.root / "log" / "development.log"
-}
+# In the development environment your application's code is reloaded on
+# every request.  This slows down response time but is perfect for development
+# since you don't have to restart the webserver when you make code changes.
+config.cache_classes = false
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = false
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_oupsnow_session',
+  :secret      => '3271ab4a527b62e55bc83792948283b0044c9df4e22c774eab635e125a43fbec4257a81b20156b9b1a866534245e98246d3587b466cd06df934176270bc1dccf'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
