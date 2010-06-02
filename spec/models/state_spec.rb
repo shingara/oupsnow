0a03e14e66664edd469cb313022391abe506b8d1
Cyril Mougel <cyril.mougel@gmail.com>
2010-06-02 18:22:13 +0200

    fix spec about state

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -5,24 +5,24 @@ describe State do
   describe 'validations' do
 
     it "should be valid" do
-      State.make.should be_valid
+      Factory(:state).should be_valid
     end
 
     it 'should not valid without name' do
-      State.make_unsaved({:name => ''}).should_not be_valid
+      Factory.build(:state, {:name => ''}).should_not be_valid
     end
 
     it 'should have unique name' do
       w = /\w+/.gen
-      State.make_unsaved({:name => w}).save.should be_true
-      State.make_unsaved({:name => w}).should_not be_valid
+      Factory.build(:state, {:name => w}).save.should be_true
+      Factory.build(:state, {:name => w}).should_not be_valid
     end
   end
 
   describe 'self#update_all_closed' do
     before do
-      @closed = State.make(:closed => true)
-      @opened = State.make(:closed => false)
+      @closed = Factory(:state, :closed => true)
+      @opened = Factory(:state, :closed => false)
     end
     it 'should change closed status of state' do
       State.update_all_closed([@closed.id, @opened.id])
